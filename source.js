230a9e5d7b1dd08cc49a185adb063d34332f06b0
John Disilwa <johndisilwa47@gmail.com>
2017-12-07 14:25:45 +0530

    first

---
+++ source.js
@@ -0,0 +1,9 @@
+//var qus = "enter ur name";
+//var ans = prompt(qus);
+var arr = ['brush','job','learn','eat','sleep'];
+var arr1 = ['ok','done','not','I will'];
+var qus1 = prompt("ans sollu 1-5");
+var qus2 = prompt('ans pannu');
+document.write('today i will ' + arr[qus1]+" and status is " +arr1[qus2] );
+//arr[0]=ans;
+//document.write(arr);
